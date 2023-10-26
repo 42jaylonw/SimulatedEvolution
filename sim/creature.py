14e4df5da17bceb378fd2ea26073ad048c8796ae
JustinVivarUCSC <jcvivar@ucsc.edu>
2023-10-26 16:00:22 -0700

    merge

--- sim/creature.py
+++ sim/creature.py
@@ -29,6 +29,8 @@ class Producer(Creature):
     def step(self):
         pass
 
+    def sense
+
 
 class Consumer(Creature):
     rgb = CONFIG['Consumer']['rgb']
@@ -36,6 +38,7 @@ class Consumer(Creature):
     def __init__(self, sim):
         super().__init__(sim)
         self.speed = CONFIG['Consumer']['init_speed']
+        self.sensory_range = CONFIG['Consumer']['sensory_range']
 
     def step(self):
         other_creatures = self.sim.creatures
@@ -43,3 +46,120 @@ class Consumer(Creature):
         d_y = np.random.choice([-self.speed, self.speed], p=[0.5, 0.5])
         self.position[0] = np.clip(self.position[0] + d_x, 0, self.sim.grid_size[0] - 1)
         self.position[1] = np.clip(self.position[1] + d_y, 0, self.sim.grid_size[1] - 1)
+
+    def sensePopulation(self):
+        """ Obtains population count (of consumers) within sensory range. """
+
+        # Obtains nearby information from function centered on the creature
+        near_info = self.sim.get_near_info(self.position, self.sensory_range)
+
+        # sums number of 1s in consumer layer of the array (if this is how the array works)
+        num_consumers = np.sum(near_info[2])
+
+        return num_consumers
+
+    def blockedFwd(self, layer, prev_move):
+        """ 
+        Returns 1 if the creature's forward direction is blocked. Returns 0 if free.
+        Orientation based on last moved direction. 
+
+        Checks on the layer specified.
+        """
+        curr_pos = self.grid_pos
+
+        if prev_move[0]:
+			next_move[0] = curr_pos[0]
+            next_move[1] = curr_pos[1] + 1
+        elif prev_move[1]:
+            next_move[0] = curr_pos[0] + 1
+            next_move[1] = curr_pos[1]
+        elif prev_move[2]:
+            next_move[0] = curr_pos[0]
+            next_move[1] = curr_pos[1] - 1
+        elif prev_move[3]:
+            next_move[0] = curr_pos[0] - 1
+            next_move[1] = curr_pos[1]
+        if self.sim.is_pos_layer_empty(layer, next_move):
+            return False
+        return True
+
+    
+    def blockedBack(self, layer, prev_move):
+        """ 
+        Returns if the creature's backward direction is blocked. Returns 0 if free.
+        Orientation based on last moved direction. 
+
+        Checks on the layer specified.
+        """
+        curr_pos = self.grid_pos
+
+        if prev_move[0]:
+            next_move[0] = curr_pos[0]
+            next_move[0] = curr_pos[1] - 1
+        elif prev_move[1]:
+            next_move[0] = curr_pos[0] - 1
+            next_move[1] = curr_pos[1]
+        elif prev_move[2]:
+            next_move[0] = curr_pos[0]
+            next_move[1] = curr_pos[1] + 1
+        elif prev_move[3]:
+            next_move[0] = curr_pos[0] + 1
+            next_move[1] = curr_pos[1]
+        if self.sim.is_pos_layer_empty(layer, next_move):
+            return False
+        return True
+
+    def blockedLeft(self, layer, prev_move):
+        """ 
+        Returns if the creature's relative left direction is blocked. Returns 0 if free.
+        Orientation based on last moved direction. 
+
+        Checks on the layer specified.
+        """
+
+        curr_pos = self.grid_pos
+
+        if prev_move[0]:
+            next_move[0] = curr_pos[0] - 1
+            next_move[0] = curr_pos[1]
+        elif prev_move[1]:
+            next_move[0] = curr_pos[0]
+            next_move[1] = curr_pos[1] + 1
+        elif prev_move[2]:
+            next_move[0] = curr_pos[0] + 1
+            next_move[1] = curr_pos[1]
+        elif prev_move[3]:
+            next_move[0] = curr_pos[0]
+            next_move[1] = curr_pos[1] - 1
+        if self.sim.is_pos_layer_empty(layer, next_move):
+            return False
+        return True
+
+    def blockedRight(self, prev_move):
+        """ 
+        Returns if the creature's relative right direction is blocked. Returns 0 if free.
+        Orientation based on last moved direction. 
+
+        Checks on the layer specified.
+        """
+        
+        curr_pos = self.grid_pos
+
+        if prev_move[0]:
+            next_move[0] = curr_pos[0] + 1
+            next_move[1] = curr_pos[1]
+        elif prev_move[1]:
+            next_move[0] = curr_pos[0]
+            next_move[1] = curr_pos[1] - 1
+        elif prev_move[2]:
+            next_move[0] = curr_pos[0] - 1
+            next_move[1] = curr_pos[1]
+        elif prev_move[3]:
+            next_move[0] = curr_pos[0] 
+            next_move[1] = curr_pos[1] + 1
+        if self.sim.is_pos_layer_empty(layer, next_move):
+            return False
+        return True
+    
+    def senseNearest():
+
