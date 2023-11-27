add21600cfb64fb6f5c51873af9c464d222b2ca3
kcmurray1 <kristiancmurray@gmail.com>
2023-11-26 20:08:12 -0800

    Made a change

--- website/static/sim-edit-menu.js
+++ website/static/sim-edit-menu.js
@@ -1,10 +1,11 @@
 
+document.addEventListener("DOMContentLoaded", function (){
+    var add_wall = document.createElement('div')
 
-var add_wall = document.createElement('div').classList.add('addWallElement');
+    add_wall.classList.add('addWallElement');
+    const placing_stuff_parent = document.querySelector('.placing-stuff');
+    placing_stuff_parent.appendChild(add_wall);
 
-const placing_stuff_parent = document.querySelector('.placing-stuff');
-
-placing_stuff_parent.appendChild(add_wall);
-
-var add_light_source;
-var add_heat_source;
+    var add_light_source;
+    var add_heat_source;
+});
